d5902768baff21a3678f61c321b16f171e6d5f98
ronny102829 <ronnyromanastoyauri@gmail.com>
2024-07-17 10:58:46 -0500

    ejercicio cinco

--- ejercicio_cinco.py
+++ ejercicio_cinco.py
@@ -4,3 +4,29 @@ ejm:
 entrada: ("jose","alvarez","30","APSTI","III")
 salida: {nombre:"jose",apellido:"alvarez",edad:"30",programa_estudio:"APSTI",semestre:"III"}
 """
+def crear_diccionario_alumno(nombre, apellido, edad, programa_estudio, semestre):
+    """
+    Retorna un diccionario con los datos personales de un alumno.
+
+    Parámetros:
+    nombre (str): El nombre del alumno.
+    apellido (str): El apellido del alumno.
+    edad (str): La edad del alumno.
+    programa_estudio (str): El programa de estudio del alumno.
+    semestre (str): El semestre del alumno.
+
+    Retorna:
+    dict: Un diccionario con los datos personales del alumno.
+    """
+    datos_alumno = {
+        "nombre": nombre,
+        "apellido": apellido,
+        "edad": edad,
+        "programa_estudio": programa_estudio,
+        "semestre": semestre
+    }
+    return datos_alumno
+
+# Ejemplo de uso
+datos = crear_diccionario_alumno("jose", "alvarez", "30", "APSTI", "III")
+print(datos)
