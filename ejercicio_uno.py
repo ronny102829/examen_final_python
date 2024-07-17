b09cdf8ac3ff4846271b7871c37014177228c8f6
ronny102829 <ronnyromanastoyauri@gmail.com>
2024-07-17 10:38:30 -0500

    ejercicio uno

--- ejercicio_uno.py
+++ ejercicio_uno.py
@@ -4,3 +4,9 @@ ejem:
 entrada: [4,7,10,4,1,0]
 salida : {menor:0,mayor:10}
 """
+def encontrar_minimo_maximo(lista):
+    minimo = min(lista)
+    maximo = max(lista)
+    return {'minimo': minimo, 'maximo': maximo}
+datos = encontrar_minimo_maximo([5, 10, 3, 8, 15])
+print(datos)
